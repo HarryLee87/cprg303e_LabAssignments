f5b3e22123773fc84a0474e8f1f5247c53828735
HarryLee87 <haewon.harry.lee@gmail.com>
2023-10-27 09:57:13 -0600

    Updated Lab assignment2

--- App.tsx
+++ App.tsx
@@ -1,91 +1,24 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * My To Do List App
  *
  * @format
  */
 
-import React, { useState } from 'react';
-
+import React from 'react';
 import {
-  Button,
-  FlatList,
-  Image,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TextInput,
-  useColorScheme,
-  View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import MyComp from './my-comp';
-
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
 
 function App(): JSX.Element {
-
-  const [inputText, setInputText] = useState('');
-
-  let condition = false;
-  let dynamicStyles = condition ? styles.text : styles.textRed;
-
-  const data = [];
-  for (let n = 0; n < 20; n++) {
-    const item = { "id": n, "text": `Item ${n}`};
-    data.push(item);    
-  }
-
-  const renderItem = ({...item}) => <Text>{item.text}</Text> 
-
-
-
-  const handlePress = () => {
-    //some code..
-  }
-
   return (
-    <View style={styles.container}>
-      <Text>CPRG_303_E: Lab Assignment 1: Spinning Up React Native App</Text>
-      <Text style={dynamicStyles}>Hello World!</Text>
-      <MyComp />
-      <Image source={require("./cat.jpg")} style={styles.image} />
-      <TextInput placeholder='Please enter your name' value={inputText} />
-      <TextInput placeholder='enter' />
-      <Button title="A Button!" onPress={handlePress} />
-      <FlatList data={data} renderItem={renderItem} />
-    </View>
+    <SafeAreaView>
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 20,
-    //alignItems: 'center'
-  },
-  text: {
-    fontSize: 20,
-    color: 'green'
-  },
-  textRed: {
-    fontSize: 20,
-    color: 'red'
-  },
-  image:{
-    width: 200,
-    height: 150,
-  }
-});
-
-
 
 export default App;
