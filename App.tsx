7554bbc3092c4a8a95250647d7ad0adea02b811d
HarryLee87 <haewon.harry.lee@gmail.com>
2023-11-03 09:48:05 -0600

    Updated Lab Assignment3: Props and State Management

--- App.tsx
+++ App.tsx
@@ -6,19 +6,30 @@
 
 import React from 'react';
 import {
+  Image,
   SafeAreaView,
+  StyleSheet,
+  View,
 } from 'react-native';
 
 import ToDoList from './ToDoList';
 import ToDoForm from './ToDoForm';
 
 function App(): JSX.Element {
+  const [tasks, setTasks] = React.useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+
+
   return (
-    <SafeAreaView>
-      <ToDoList />
+    <SafeAreaView>      
+      <ToDoList tasks={tasks} />
       <ToDoForm />
     </SafeAreaView>
   );
 }
+
 
 export default App;
--- ToDoList.jsx
+++ ToDoList.jsx
@@ -3,29 +3,35 @@ import {
     ScrollView, 
     Pressable, 
     View, 
-    Text } from "react-native";
+    Text, 
+        } from "react-native";
 
 
 
-export default function ToDoList(){
+export default function ToDoList({ tasks }){
 
     return(
         <ScrollView>
-        <Pressable>
-          <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Do laundry</Text>
-          </View>
-        </Pressable>
-        <Pressable>
-          <View style={[styles.task]}>
-            <Text style={styles.taskText}>Go to gym</Text>
-          </View>
-        </Pressable>
-        <Pressable>
-          <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Walk dog</Text>
-          </View>
-        </Pressable>
+
+        {tasks.map((task, index) =>
+          <Pressable key={index}>
+            <View style={[styles.task]}>
+              <Text style={styles.taskText}>
+                {task}
+              </Text>
+            </View>
+          </Pressable>
+          )}
+
+          {/* <Pressable>
+            {tasks.map((task) => (
+              <View style={[styles.task]}>
+                <Text style={styles.taskText}>
+                  {task}
+                </Text>
+              </View>
+            ))}
+          </Pressable> */}
       </ScrollView>
 
     );
